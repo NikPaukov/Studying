9aabb418a8950afdf966577b84fee7d6c308951f
NikPaukov <sirelfarionik@gmail.com>
2022-05-12 21:09:06 +0300

    sda

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,23 @@
 'use strict'
 
+//func
 function toAnonym(text) {
-    const textArr = text.split(" ");
-    console.log(textArr);
+    const textArr = text.split(/\s+/);
+    for(let i = 1; i < textArr.length; i++) {
+        if(textArr[i][0].match(/[A-Z]/) && textArr[i-1][textArr[i-1].length-1] != "."){
+             if(textArr[i][textArr[i].length-1] =="."){
+                 textArr[i] = "writer.";}
+            else textArr[i] ="writer"; 
+        }
+       
+    }
+    return textArr.join(" ");
 }
+   
+//out
+const inpText = `Highly recommend you to read at least one work of Shakespeare. He made his masterpieces for hundreds of years before London, Dickens and Fitzgerald. Hope to buy last book to my collection it 2022, and read it.`
+console.log(toAnonym(inpText));
+function getParsed(textId, outId) {
+    const text = document.getElementById(textId).value;
+    document.getElementById(outId).textContent = toAnonym(text);
+    }
